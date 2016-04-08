cec3b0146dde386552d80f93b6aed826cdbc4242
Chad Hollenbeck <HOLLENCH@mail.gvsu.edu>
2016-04-08 09:51:59 -0400

    Paint works, chat work, just need to sync with new users

---
+++ SignalRDemo/SignalRDemo/connect.js
@@ -0,0 +1,126 @@
+﻿$(function () {
+    //Initial Setup
+    Init();
+
+    //Create reference to the hub
+    var conn = $.connection.sigHub;
+
+    //-----Connection Functions------//
+    //-----Update from server--------//
+    conn.client.sendmessage = function (username, message) //send chat messages
+    {
+        var encodedName = $('<div />').text(username).html();
+        var encodedMsg = $('<div />').text(message).html();
+        // Add the message to the page. 
+        $('#discussion').append('<li><strong>' + encodedName
+            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+
+        //Force chat window to scroll down on new content
+        var out = $('.message-frame');
+        // allow 1px inaccuracy by adding 1
+        var isScrolledToBottom = out.scrollHeight - out.clientHeight <= out.scrollTop + 1;
+        if (isScrolledToBottom)
+            out.scrollTop = out.scrollHeight - out.clientHeight;
+    }
+
+    conn.client.redraw = function (cellID, color) //Update table cells
+    {
+        $('#' + cellID).removeClass();
+        $('#' + cellID).addClass(color);
+    }
+
+    //----------Start Connection-----------//
+    //-----------Push new data TO server---//
+    $.connection.hub.start().done(function () {
+        //Login
+        $(function () {
+            conn.server.login($('#displayname').val());
+        });
+
+        //Bind Dragover for color application
+        $('th').bind('dragenter', function (ev) {
+            conn.server.redraw(ev.target.id, $('#colors option:selected').text());
+        });
+        $('th').bind('dragstart', function (ev) {
+            conn.server.redraw(ev.target.id, $('#colors option:selected').text());
+        });
+
+
+
+        //Chat
+        $('#sendmessage').click(function () {
+            // Call the SendMessage method on the hub. 
+            conn.server.send($('#displayname').val(), $('#message').val());
+
+            // Clear text box and reset focus for next comment. 
+            $('#message').val('').focus();
+        });
+
+        //Draw
+        $('th').click(function (ev) {
+            var cellID = ev.target.id;
+            var color = $('#colors option:selected').text();
+
+            conn.server.redraw(cellID, color);
+        });
+
+
+    });
+
+});
+
+//Functions outside of connection
+function Init() {
+
+    //populate the board
+    $('#drawBoard').html(createBoard());
+    //Get username from user
+    var user = prompt("Please enter a username");
+
+    while (user === "" || user === null)
+    {
+        user = prompt('Please Enter a Username:')
+    }
+    $('#displayname').val(user);
+
+    //Set focus on message board
+    $('#message').focus();
+
+    //Bind Enter Key to Button Press
+    $(document).keypress(function (e) {
+        if(e.which == 13)
+        {
+            $('#sendmessage').click();
+        }
+    });
+
+    
+}
+
+function createBoard() {
+    var id = 1;
+    var tableHTML = '<table><tbody><tr>';
+    var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z','AA','AB','AC','AD','AE','AF','AG','AH','AJ','AK'];
+
+    letters.forEach(function(element)
+    {
+        tableHTML += '<th>' + element + '</th>';
+    })
+
+    tableHTML += '</tr>';
+    for (var r = 1; r <= 22; r++) {
+
+        tableHTML += '<tr><th>' + r + '</th>';
+
+        //First Row and column are Headers
+        for (var i = 0; i < letters.length-1; i++) {
+            tableHTML += '<th id="' + letters[i] + id + '" class=""></th>';
+        }
+
+        tableHTML += '</tr>';
+        id++;
+
+    }
+
+    return tableHTML;
+}
---
+++ SignalRDemo/SignalRDemo/hubs/SigHub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo.hubs
+{
+    public class SigHub:Hub
+    {
+
+            /// <summary>
+            /// Sends message through the chat interface
+            /// </summary>
+            /// <param name="username">taken from user input</param>
+            /// <param name="message">taken from user input</param>
+            public void Send(string username, string message)
+            {
+                Clients.All.sendmessage(username, message);
+            }
+
+            /// <summary>
+            /// Uses the send Message function to display when a user logs in
+            /// Also add's their name to a list of users
+            /// </summary>
+            /// <param name="username"></param>
+            public void Login(string username)
+            {
+            Send(username, "logged in");
+            }
+
+            /// <summary>
+            /// use SendMessage to alert the chat board
+            /// Remove user from list
+            /// </summary>
+            /// <param name="username"></param>
+            public void Logout(string username)
+            {
+            Send(username, "logged out");
+            }
+
+            /// <summary>
+            /// Synchronize new users with table and chat data
+            /// </summary>
+            public void Synchronize()
+            {
+
+            }
+
+            /// <summary>
+            /// Update the Table with each click
+            /// </summary>
+            public void Redraw(string cellID, string color)
+            {
+                Clients.All.redraw(cellID, color);
+            }
+        }
+}
